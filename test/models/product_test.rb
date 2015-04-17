61e98a8e1b0f9eec3344673085965353567639e2
andychups <andychups@ya.ru>
2015-04-17 18:40:35 +0000

    Тестики-хуестики на моделечку-пизделичку

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def new_product(title, desc, image_url, price)
+    Product.new(title: title, desc: desc, image_url: image_url, price: price)
+  end
+
+  def new_filled_product(title: 'Title', desc: 'Desc', image_url: 'image.png', price: 22)
+    new_product(title, desc, image_url, price)
+  end
 
   test "product attributes must not be empty" do
     product = Product.new
@@ -15,7 +19,7 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test "product price must be positive" do
-    product = Product.new(title: 'Title', desc: 'Desc', image_url: 'image.png', price: 22)
+    product = new_filled_product()
 
     product.price = -1
     assert product.invalid?
@@ -26,6 +30,19 @@ class ProductTest < ActiveSupport::TestCase
     assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
 
     product.price = 22
-    assert product.valid?
+    assert product.valid?, 'product is invalid'
+  end
+
+  test "image_url" do
+    correct = %w{i.gif i.png i.jpg i.jpeg I.GIF I.PNG I.JPG I.JPEG i.JpEg http://www.yandex.ru/some/folder/i.png}
+    incorrect = %w{i.bmp I.BMP i.bmp.gif.png2000 i.png.tmp}
+
+    correct.each do |value|
+      assert new_filled_product(image_url: value).valid?, "#{value} shouldn't be invalid"
+    end
+
+    incorrect.each do |value|
+      assert new_filled_product(image_url: value).invalid?, "#{value} shouldn't be valid"
+    end
   end
 end
