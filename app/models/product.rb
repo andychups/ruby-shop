d5a8c1641db05e637c8af68a8ea5d382f8e0f7dd
andychups <andychups@ya.ru>
2015-04-19 19:25:59 +0000

    Лейаут, кеширование, тесты

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,8 @@ class Product < ActiveRecord::Base
       with: %r{\.(gif|jpg|jpeg|png)\Z}i,
       message: 'Incorrect image path'
   }
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
 end
--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,11 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '.layout .header nav', 1
+    assert_select '.layout .content', 1
+    assert_select '.layout .footer', 1
+    assert_select '.catalog .catalog__item', 3
+    assert_select '.catalog .catalog__item:first-of-type td', 4
   end
 
 end
