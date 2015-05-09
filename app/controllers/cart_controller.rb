599ffb9785b308f7c7b894becbe6b33ba6b6b9bc
andychups <andychups@ya.ru>
2015-05-09 16:38:21 +0000

    Удаление корзины и обработка кейсы с пустой корзиной

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,11 +2,13 @@ class CartController < ApplicationController
 
   # GET /cart
   def index
-    @cart = Cart.find(session[:cart_id])
+    @cart = (session[:cart_id]) ? Cart.find(session[:cart_id]) : nil
     @total_price = 0
 
-    @cart.line_items.each do |item|
-      @total_price += item.quantity * item.product.price
+    if (@cart)
+      @cart.line_items.each do |item|
+        @total_price += item.quantity * item.product.price
+      end
     end
   end
 end
