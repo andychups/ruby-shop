7e3d01c5f1c452fc10388845ad6fd174041aee79
andychups <andychups@ya.ru>
2015-05-10 17:29:27 +0000

    Рефакторинг общей цены

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -3,12 +3,6 @@ class CartController < ApplicationController
   # GET /cart
   def index
     @cart = (session[:cart_id]) ? Cart.find(session[:cart_id]) : nil
-    @total_price = 0
-
-    if (@cart)
-      @cart.line_items.each do |item|
-        @total_price += item.quantity * item.product.price
-      end
-    end
+    @total_price = @cart ? @cart.total_price : nil
   end
 end
